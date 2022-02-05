53450ef0e48e36d64fd24f01dab749041b3c00ae
Huy Le <huyftw@aim.com>
2022-02-04 21:52:06 -0500

    changed publish type to test code

--- src/ir_sensor_pkg/ir_sensor_pkg/ir_dist_publisher.py
+++ src/ir_sensor_pkg/ir_sensor_pkg/ir_dist_publisher.py
@@ -3,6 +3,8 @@ import rclpy
 from rclpy.node import Node
 import spidev
 
+from example_interfaces.msg import String
+
 """
     source code for ir sensor and modified for ros implementation from:
     https://tutorials-raspberrypi.com/infrared-distance-measurement-with-the-raspberry-pi-sharp-gp2y0a02yk0f/
@@ -20,13 +22,15 @@ class ir_dist_publisher_node(Node):
         self.dist = 16.2537 * v**4 - 129.893 * v**3 + 382.268 * v**2 - 512.611 * v + 301.439
 
         self.publish_dist = self.create_publisher(
-            msg_type=float, topic="ir_distance", qos_profile=10)
+            msg_type=String, topic="ir_distance", qos_profile=10)
         self.timer = self.create_timer(1.0, self.publish_dist)
 
         self.get_logger().info("ir sensor publisher started.")
 
     def publish_distance(self):
-        self.publish_dist.publish(str(self.dist))
+        msg = String()
+        msg.data = "hello world"
+        self.publish_dist.publish(msg)
 
     def ReadChannel(self, channel):
         val = ir_sensor.xfer2([1, (8+channel) << 4, 0])
