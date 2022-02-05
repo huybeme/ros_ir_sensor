6ed526f2dc304cdb7d47de6f2c8e4542e18c1ef7
Huy Le <huyftw@aim.com>
2022-02-04 22:36:24 -0500

    add publisher to listener

--- src/ir_sensor_pkg/ir_sensor_pkg/ir_dist_listener.py
+++ src/ir_sensor_pkg/ir_sensor_pkg/ir_dist_listener.py
@@ -3,18 +3,26 @@ import rclpy
 from rclpy.node import Node
 
 from example_interfaces.msg import String
+from example_interfaces.msg import Float64
 
 
 class ir_distance_listener_node(Node):
     def __init__(self):
         super().__init__("ir_dist_listener")
+        self.distance = 0.0
+        self.publisher_dist = self.create_publisher(
+            Float64, "ir_distance_values", 10)
+
 
         self.subscriber_ = self.create_subscription(
             String, "ir_distance", self.callback_ir_distances, 10)
         self.get_logger().info("listening to ir sensor...")
 
     def callback_ir_distances(self, msg):
-        self.get_logger().info(msg.data)
+        # self.get_logger().info(msg.data)
+        dist_msg = Float64()
+        dist_msg.data = msg.data
+        self.publisher_dist.publish(dist_msg)
 
 def main(args=None):
     rclpy.init(args=args)
